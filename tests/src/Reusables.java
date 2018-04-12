ecb21126c2f8df5138813f2abb26bd2f6cde1c4f
pegah <palipoor976@gmail.com>
2018-04-12 17:46:47 +0430

    شروع نوشتن توابع ری یوزبل

--- tests/src/Reusables.java
+++ tests/src/Reusables.java
@@ -3,7 +3,14 @@ import org.openqa.selenium.WebDriver;
 /**
  * Created by Golpar on 4/12/2018 AD.
  */
-public class Reusables {
+
+/// این کلاس برای تامین توابع دوباره استفاده کردنی در بخش‌های مختلف تست‌ها ساخته شده. کافی ست این کلاس در فایل هر تستی اینکلود بشه و از توابع
+    // استاتیکش استفاده بشه.
+    // صرفا باید وبدراویر رو بهش بدیم تا عوضش کنه به جایی که باید.
+
+    // در توابعی که قرار نیست وبدرایور عوض بشه، باید برای استفاده یک وبدرایور ساخت و بهش داد.
+
+public class ReUsables {
 
     public static void loginAsACustomer(WebDriver homepage){
     }
@@ -18,12 +25,12 @@ public class Reusables {
 
     }
 
-    public static int getPriceFromPanel(WebDriver panel, String currency){ // could be "dollar" or "euro"
+    public static int getPrice(WebDriver homepage, String currency){ // حتما بهش یک وبدرایور جدید که آدرس هومپیج رو باز کرده بدین! به باد می‌رین مگرنه.
         return 0;
     }
-    public static int getPriceFromLandingPage(WebDriver panel, String currency){
+
+    public static int getWalletCredit(WebDriver panel, String currency){ // currency parameter could be "dollar" or "euro" or "rial"
         return 0;
     }
-    
 
 }
