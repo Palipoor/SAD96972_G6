027db66599b85033c50519b421c0b2f488a1fe58
pegah <palipoor976@gmail.com>
2018-04-12 17:35:29 +0430

    اولین کامیت. شروع کردم توابع رییوزبل رو تعریف کردن.

---
+++ tests/src/Reusables.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Golpar on 4/12/2018 AD.
+ */
+public class Reusables {
+
+    public static void loginAsACustomer(WebDriver homepage){
+    }
+
+    public static void loginAsTheManager(WebDriver homepage){
+    }
+
+    public static void loginAsAnEmployee(WebDriver homepage){
+    }
+
+    public static void logout(WebDriver panel){
+
+    }
+
+    public static int getPriceFromPanel(WebDriver panel, String currency){ // could be "dollar" or "euro"
+        return 0;
+    }
+    public static int getPriceFromLandingPage(WebDriver panel, String currency){
+        return 0;
+    }
+    
+
+}
