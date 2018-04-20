9550fdfce17eeee948edf8a46d7576ff1a57685b
Moeen <smnerhzati@gmail.com>
2018-04-20 15:38:02 +0430

    script uses function

--- website/handle_static.py
+++ website/handle_static.py
@@ -9,6 +9,10 @@ if(len(sys.argv) > 1):
     root = sys.argv[1]
 if (len(sys.argv) > 2):
     static_to_root = sys.argv[2]
+address_input_for_function = ""
+def replace(match):
+    result = match.group("tag") + r''' = '{% static "''' + os.path.normpath(address_input_for_function+match.group('address')) + r'''" %}' '''
+    return result
 link_reg = re.compile(r"""(?P<tag>href|src)\s*=\s*['"](?!http)(?!{%)(?P<address>.*?)[\'\"]""",re.DOTALL)
 for dirpath, dnames, fnames in os.walk(root):
     for file_name in fnames:
@@ -18,11 +22,10 @@ for dirpath, dnames, fnames in os.walk(root):
         if (file_name.endswith('.html')):
             with open(file_address) as html:                
                 code = html.read()
-                print('hey')
                 if(not code.startswith("{%")):
                     code = r"{% load static staticfiles %}" + "\n" + code
-                print(r'''\g<tag> = '{% static "''' + static_to_root + root_to_html + r'''\g<address>" %}' ''')
-                code = link_reg.sub(r'''\g<tag> = '{% static "''' + root_to_html + r'''\g<address>" %}' ''', code)
+                address_input_for_function = static_to_root + root_to_html
+                code = link_reg.sub(replace, code)
                 # print(link_reg.findall(code))
             with open(file_address, 'wt') as html:
                 html.write(code)
