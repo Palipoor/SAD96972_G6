28542d5f436fd5bd5a50b3d4cce4b2a8f5f7a60a
pegah <palipoor976@gmail.com>
2018-04-26 10:26:20 +0430

    conversion test

--- tests/src/Nonmembers/Laws.java
+++ tests/src/Nonmembers/Laws.java
@@ -11,23 +11,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Golpar on 4/21/2018 AD.
+ * Created by Golpar on 4/26/2018 AD.
  */
-public class AboutUs {
+public class Laws {
+
     private static WebDriver driver;
 
     @BeforeClass
     public static void setUp() {
+
         driver = new ChromeDriver();
         GeneralReusables.setUpToHomepage(driver);
-        WebElement about = driver.findElement(By.name("about"));
-        about.click();
+        WebElement laws = driver.findElement(By.name("law"));
+        laws.click();
     }
 
     @Test
     public void containsText() {
-        WebElement title = driver.findElement(By.name("about-us-header"));
-        WebElement content = driver.findElement(By.name("about-us-content"));
+        WebElement title = driver.findElement(By.name("laws-header"));
+        WebElement content = driver.findElement(By.name("laws-content"));
         assertTrue(!title.getText().equals("") && !content.getText().equals(""));
     }
 
