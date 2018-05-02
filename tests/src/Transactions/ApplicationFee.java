54f103963c06902f975b24a9d155ca627449148c
pegah <palipoor976@gmail.com>
2018-05-02 13:16:02 +0430

    ghable merge ba dorna

--- tests/src/Transactions/ApplicationFee.java
+++ tests/src/Transactions/ApplicationFee.java
@@ -1,6 +1,7 @@
 package Transactions;
 
 import Reusables.GeneralReusables;
+import Reusables.WalletUsersReusables;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by Golpar on 5/2/2018 AD.
@@ -53,18 +56,74 @@ public class ApplicationFee {
         submit.click();
 
         WebElement error = driver.findElement(By.name("amount-error"));
-
-
+        assertEquals(error.getText(), GeneralReusables.reusableStrings.get("wrong-amount-error"));
     }
 
     @Test
     public void moreThanCredit() {
+        int moreThanDollarCredit = WalletUsersReusables.getWalletCredit(driver, "dollar") + 3;
+        String paymentAmount = String.valueOf(moreThanDollarCredit);
 
+        WebElement universityName = driver.findElement(By.name("university-name"));
+        universityName.clear();
+        universityName.sendKeys("ETHZ");
+
+        WebElement amount = driver.findElement(By.name("amount"));
+        amount.clear();
+        amount.sendKeys(paymentAmount);
+
+        WebElement currency = driver.findElement(By.name("dollar-radio"));
+        currency.click();
+
+        WebElement link = driver.findElement(By.name("link"));
+        link.clear();
+        link.sendKeys("www.ethz.ch");
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.clear();
+        username.sendKeys("myusername");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.clear();
+        password.sendKeys("myusername");
+
+        WebElement submit = driver.findElement(By.name("submit-button"));
+        submit.click();
+
+        WebElement error = driver.findElement(By.name("error"));
+        assertEquals(error.getText(), WalletUsersReusables.reusableStrings.get("not-enough-error"));
     }
 
     @Test
     public void transactionIsAdded() {
 
+        String paymentAmount = "1";
+
+        WebElement universityName = driver.findElement(By.name("university-name"));
+        universityName.clear();
+        universityName.sendKeys("ETHZ");
+
+        WebElement amount = driver.findElement(By.name("amount"));
+        amount.clear();
+        amount.sendKeys(paymentAmount);
+
+        WebElement currency = driver.findElement(By.name("dollar-radio"));
+        currency.click();
+
+        WebElement link = driver.findElement(By.name("link"));
+        link.clear();
+        link.sendKeys("www.ethz.ch");
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.clear();
+        username.sendKeys("myusername");
+
+        WebElement password = driver.findElement(By.name("password"));
+        password.clear();
+        password.sendKeys("myusername");
+
+        WebElement submit = driver.findElement(By.name("submit-button"));
+        submit.click();
     }
 
     @AfterClass
