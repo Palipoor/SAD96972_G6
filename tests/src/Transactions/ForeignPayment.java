8ea219488cb80fc59f59f9dc9fa8e55039a46937
pegah <palipoor976@gmail.com>
2018-05-02 17:31:23 +0430

    darkhaste yek tarakonesh test shod

--- tests/src/Transactions/ForeignPayment.java
+++ tests/src/Transactions/ForeignPayment.java
@@ -10,14 +10,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by Golpar on 5/2/2018 AD.
  */
-public class AnonymousPayment {
+public class ForeignPayment {
     private static WebDriver driver;
 
     @BeforeClass
@@ -25,8 +24,8 @@ public class AnonymousPayment {
         driver = new ChromeDriver();
         GeneralReusables.setUpToHomepage(driver);
         GeneralReusables.loginAsACustomer(driver);
-        WebElement anonymousPayment = driver.findElement(By.name("anonymous-payment"));
-        anonymousPayment.click();
+        WebElement foreignPayment = driver.findElement(By.name("foreign-payment"));
+        foreignPayment.click();
     }
 
     @Test
@@ -35,68 +34,64 @@ public class AnonymousPayment {
         amount.clear();
         amount.sendKeys("abc");
 
-        WebElement email = driver.findElement(By.name("email"));
-        email.clear();
-        email.sendKeys("palipoor976@gmail.com");
+        WebElement currency = driver.findElement(By.name("dollar-radio"));
+        currency.click();
 
-        WebElement phone = driver.findElement(By.name("phone"));
-        phone.clear();
-        phone.sendKeys("09379605628");
+        WebElement accountNumber = driver.findElement(By.name("destination"));
+        accountNumber.clear();
+        accountNumber.sendKeys("1234567890");
 
-        WebElement button = driver.findElement(By.name("submit-button"));
-        button.click();
+        WebElement submit = driver.findElement(By.name("submit-button"));
+        submit.click();
 
-        WebElement error = driver.findElement(By.name("error"));
-        assertEquals(error.getText(), GeneralReusables.reusableStrings.get("wrong-amount-error"));
+        WebElement error = driver.findElement(By.name("amount-error"));
+        assertEquals(error.getText(), GeneralReusables.reusableStrings.get("invalid-amount-error"));
     }
 
     @Test
-    public void invalidEmail() {
+    public void invalidDestination() {
         WebElement amount = driver.findElement(By.name("amount"));
         amount.clear();
-        amount.sendKeys("1000");
+        amount.sendKeys("10");
 
-        WebElement email = driver.findElement(By.name("email"));
-        email.clear();
-        email.sendKeys("palipoor976");
+        WebElement currency = driver.findElement(By.name("dollar-radio"));
+        currency.click();
 
+        WebElement accountNumber = driver.findElement(By.name("destination"));
+        accountNumber.clear();
+        accountNumber.sendKeys("1");
 
-        WebElement button = driver.findElement(By.name("submit-button"));
-        button.click();
+        WebElement submit = driver.findElement(By.name("submit-button"));
+        submit.click();
 
-        WebElement error = driver.findElement(By.name("error"));
-        assertEquals(error.getText(), GeneralReusables.reusableStrings.get("invalid-email-error"));
+        WebElement error = driver.findElement(By.name("amount-error"));
+        assertEquals(error.getText(), GeneralReusables.reusableStrings.get("invalid-account-number-error"));
     }
 
     @Test
     public void moreThanCredit() {
-        double moreThanRialCredit = WalletUsersReusables.getWalletCredit(driver, "rial") + 200;
-        String amountValue = String.valueOf(moreThanRialCredit);
+        double moreThanDollarCredit = WalletUsersReusables.getWalletCredit(driver, "dollar") + 3;
+        String paymentAmount = String.valueOf(moreThanDollarCredit);
+
         WebElement amount = driver.findElement(By.name("amount"));
         amount.clear();
-        amount.sendKeys(amountValue);
-        WebElement email = driver.findElement(By.name("email"));
-        email.clear();
-        email.sendKeys("palipoor976@gmail.com");
+        amount.sendKeys(paymentAmount);
 
-        WebElement phone = driver.findElement(By.name("phone"));
-        phone.clear();
-        phone.sendKeys("09379605628");
-
-        WebElement button = driver.findElement(By.name("submit-button"));
-        button.click();
+        WebElement submit = driver.findElement(By.name("submit-button"));
+        submit.click();
 
         WebElement error = driver.findElement(By.name("error"));
         assertEquals(error.getText(), WalletUsersReusables.reusableStrings.get("not-enough-error"));
+
     }
 
     @Test
     public void paymentsAreDone() {
-
-        String paymentAmount = "1000";
+        String paymentAmount = "1";
+        double dollarCredit = WalletUsersReusables.getWalletCredit(driver, "dollar");
         double rialCredit = WalletUsersReusables.getWalletCredit(driver, "rial");
         double companyRialCredit = ManagerReusables.getCompanyCredit("rial");
-
+        double companyDollarCredit = ManagerReusables.getCompanyCredit("dollar");
         WebElement amount = driver.findElement(By.name("amount"));
         amount.clear();
         amount.sendKeys(paymentAmount);
@@ -111,8 +106,10 @@ public class AnonymousPayment {
         double newCompanyRialCredit = ManagerReusables.getCompanyCredit("rial");
         double newCompanyDollarCredit = ManagerReusables.getCompanyCredit("dollar");
 
-        assertEquals(newRialCredit, rialCredit - karmozdValue - 1000.0, GeneralReusables.delta);
-        assertEquals(newCompanyRialCredit, companyRialCredit + karmozdValue + 1000.0, GeneralReusables.delta);
+        assertEquals(newDollarCredit, dollarCredit - 1.0, GeneralReusables.delta);
+        assertEquals(newRialCredit, rialCredit - karmozdValue, GeneralReusables.delta);
+        assertEquals(newCompanyDollarCredit, companyDollarCredit + 1.0, GeneralReusables.delta);
+        assertEquals(newCompanyRialCredit, companyRialCredit + karmozdValue, GeneralReusables.delta);
     }
 
     @Test
