79d09330753b9f3b7f788bf05ee7e1a8d1de43b3
pegah <palipoor976@gmail.com>
2018-05-02 13:49:42 +0430

    merged

--- tests/src/Reusables/OrderedRunner.java
+++ tests/src/Reusables/OrderedRunner.java
@@ -3,7 +3,6 @@
  */
 package Reusables;
 
-import CustomersTransactions.Order;
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
