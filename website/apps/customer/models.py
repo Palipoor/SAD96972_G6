99df73fe48ce32e7b61b4e33e8357a5131468aed
Dornle <dorna.abdolazimi@gmail.com>
2018-06-02 04:37:17 +0430

    3 of other transactions  added

--- website/apps/customer/models.py
+++ website/apps/customer/models.py
@@ -8,7 +8,7 @@ class Customer(main.models.GenUser):
     rial_credit = models.IntegerField()
     dollar_cent_credit = models.IntegerField()
     euro_cent_credit = models.IntegerField()
-    account_number = models.IntegerField(unique=True, null=False)
+    account_number = models.CharField(max_length=20, unique=True, null=False)
 
 
 class Transaction(models.Model): #???
@@ -25,9 +25,9 @@ class Transaction(models.Model): #???
         (2, 'euro'),
     )
     customer = models.ForeignKey('Customer', on_delete=models.DO_NOTHING)
-    currency = models.IntegerField(choices=currency)
-    amount = models.IntegerField()
-    request_time = models.DateTimeField()
+    currency = models.IntegerField(choices=currency, null=False)
+    amount = models.IntegerField(null=False)
+    request_time = models.DateTimeField(null=False)
     description = models.CharField(max_length=500)
     #status = models.IntegerField(choices=statuses)
     #destination = models.IntegerField()
@@ -50,8 +50,8 @@ class TestTrans(Transaction):
         class Meta:
             abstract = True
 
-        username = models.CharField(max_length=100)
-        password = models.CharField(max_length=50)
+        username = models.CharField(max_length=100, null=False)
+        password = models.CharField(max_length=50, null=False)
 
 
 class IBT(TestTrans):
@@ -59,15 +59,15 @@ class IBT(TestTrans):
         abstract = True
 
 
-    test_center_name = models.CharField(max_length=100)
-    test_center_code = models.IntegerField(max_length=50)
-    city = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    date = models.DateTimeField()
+    test_center_name = models.CharField(max_length=100, null=False)
+    test_center_code = models.IntegerField(max_length=50, null=False)
+    city = models.CharField(max_length=100, null=False)
+    country = models.CharField(max_length=100, null=False)
+    date = models.DateTimeField(null=False)
 
 
 class TOEFL(IBT):
-    idTypes = (
+    id_types = (
         (0, 'Passport'),
         (1, 'National ID'),
 
@@ -85,7 +85,7 @@ class TOEFL(IBT):
         (9, 'blah'),
 
     )
-    countriesForStudying = (
+    countries_for_studying = (
         (0, 'blah'),
         (1, 'blah'),
         (2, 'blah'),
@@ -99,10 +99,11 @@ class TOEFL(IBT):
 
     )
 
-    reason = models.IntegerField(choices=reasons) #handle several reasons
-    countryForStudy = models.IntegerField(choices=countriesForStudying)  # handle several country
-    IDType = models.IntegerField(choices=idTypes)
-    ID = models.IntegerField()
+    reason = models.IntegerField(choices=reasons, null=False) #handle several reasons
+    country_for_study = models.IntegerField(choices=countries_for_studying, null=False)  # handle several country
+    id_type = models.IntegerField(choices=id_types, null=False)
+    id = models.CharField(max_length=20)
+
 
 class GRE(IBT):
     citizenships = (
@@ -121,8 +122,30 @@ class GRE(IBT):
         (7, 'blah'),
 
     )
-    majorFiledCode = models.IntegerField()
-    majorFiledName = models.CharField(max_length=50)
-    citizenship = models.IntegerField(choises=citizenships)
-    educationalStatus = models.IntegerField(choices=statuses)
+    major_filed_code = models.IntegerField(null=False)
+    major_filed_name = models.CharField(max_length=50, null=False)
+    citizenship = models.IntegerField(choices= citizenships, null=False)
+    educational_status = models.IntegerField(choices=statuses, null=False)
 
+
+class UniversityTransaction(Transaction):
+        types=(
+            (0, 'Application Fee'),
+            (1, 'Deposit Fee')
+        )
+        type = models.IntegerField(choices=types, null=False)
+        university_name = models.CharField(max_length=50, null=False)
+        link = models.URLField(null=False)
+        username = models.CharField(max_length=100, null=False)
+        password = models.CharField(max_length=50, null=False)
+        guide = models.CharField(max_length=1000, null=False)
+
+
+class ForeignTrans(Transaction):
+    account_number = models.CharField(max_length=20,  null=False)
+    bank_name = models.CharField(max_length=50,  null=False)
+
+
+class InternalTrans(Transaction):
+    account_number = models.CharField(max_length=20)
+    bank_name = models.CharField(max_length=50)
