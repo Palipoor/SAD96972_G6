af60b678088897863f54bdc3c8544aa23d5c66a8
pegah <palipoor976@gmail.com>
2018-08-13 14:35:36 +0430

    Fix employee settings

--- website/apps/employee/Forms.py
+++ website/apps/employee/Forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import ModelForm
+from utils import fields
 
 from apps.employee.models import Employee
 
@@ -13,3 +14,8 @@ class EmployeeSettingsForm(ModelForm):
     class Meta:
         model = Employee
         fields = ['username', 'first_name', 'last_name', 'persian_first_name', 'persian_last_name']
+    username = fields.USERNAME
+    first_name = fields.FIRST_NAME
+    last_name = fields.LAST_NAME
+    persian_first_name = fields.PERSIAN_FIRST_NAME
+    persian_last_name = fields.PERSIAN_LAST_NAME
